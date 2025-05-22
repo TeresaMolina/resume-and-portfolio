40d75070e45e8daba56d00b31095662c8e40a6fa
Teresa Molina <teresa.molina01@utrgv.edu>
2025-05-22 17:53:09 -0500

    added descriptions

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -12,12 +12,65 @@ function Education() {
 
       <p><b>HCISD Student Help Desk - 9th-12th</b></p>
       <p class="italic"> <b>Description:</b> the first of many student tech teams of HCISD that do volunteer work for the school district's events need technical assistance.</p>
+      <p>&nbsp;</p>
+<p><b>Skills</b></p>
       <ul>
         <li>Customer Service & Troubleshooting 101 Certified</li>
         <li>Assisted in District Panel Interviews for principals and teachers</li>
         <li>Assisted and Presented in District STEM events</li>
         <li>Presented and Lead in the Student-By-Student Technology and Leadership Conferences 9th-11th (12th was canceled pandemic year)</li>
       </ul>
+                  <p>&nbsp;</p>
+      <p><b>HSHP Student Tech Team 9th-12th</b></p>
+            <h5>Elective wanting to showcase: Computer Science 101</h5>
+
+      <p class="italic"><b>Description: </b>along with district, I was also the leader of our school's Student Tech Team from beginning to the Pandemic of 2020</p>
+    <p>&nbsp;</p>
+<p><b>Skills</b></p>
+    <ul>
+      <li> HTML Coding </li>
+      <li> PC Maintenance & Repair </li>
+      <li> PC Build & Clean Up </li>
+      <li> In-School STEM events </li>
+      <li> Technology Projects </li>
+      <li> Technical Support </li>
+      <li> Pi set up and use </li>
+      <li> Wire Control </li>
+      <li> Block Coding </li>
+
+    </ul>
+                         <p>&nbsp;</p>
+
+      <h2 class="center">《South Texas Colege '23》</h2>
+     <h5 class="center">Associate's of Computer Science</h5>
+           <hr class="rounded"></hr>
+    <p><b>Skills Obtained</b></p>
+    <p>Coding in: C++ | C# | C in Linux Environment | Assembly</p>
+
+                         <p>&nbsp;</p>
+
+      <h2 class="center">《UTRGV '26》</h2>
+     <h5 class="center">Bachelor's of Science in Compuer Science - General Major
+</h5>
+           <hr class="rounded"></hr>
+
+ <p><b>Courses Worth Mentioning</b></p>
+<ul>
+  <li>Web Development</li>
+  <li>Game Development</li>
+  <li>Game and Computation</li>
+  <li>Organization of Programming Languages</li>
+</ul>
+
+<p>&nbsp;</p>
+<p><b>Skills</b></p>
+
+<ul>
+  <li><b>Coding in:</b> HTML + CSS + JS | C++ | C#</li>
+  <li><b>Coded with:</b> Object-Oriented Programming in JAVA | Haskell | Objective-C</li>
+  <li>Use of Blender for sculpture edits or fix ups</li>
+  <li>Use of Photoshop for image fixes/render</li>
+</ul>
 
     </div>
   );
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,8 +3,54 @@ import './pages.css';
 function Experience() {
   return (
     <div>
-      <h2>Work Experience</h2>
-      <p>University Help Desk Assistant for LMS Support (Brightspace & Blackboard)</p>
+      <h2 class="center">Center for Online Learning & Teaching Technology</h2>
+     <h5 class="center">Student Help Desk Assistant</h5>
+
+           <hr class="rounded"></hr>
+           <p>&nbsp;</p>
+     <p class="italic"><b>Description: </b> Assists in the technical support of 
+                              the Learning Managment Systems Blackboard and Brightspace for
+                                faculty, staff, and students. Tasks range from troubleshooting to
+                                  aiding in third-party tool links and course content. </p>
+    <p>&nbsp;</p>
+    <ul>
+      <li> Blackboard LMS Guided Training </li>
+      <li> Brightspace LMS Guided Training </li>
+      <li> Participant for UTRGV's migration from LMS Blackboard to Brightspace </li>
+      <li> Participant to Designated Knowledge-Based Article editor for Brightspace Support - Using Adobe Illustrator and HTML Experience </li>
+      <li> Participant in Echo Labs Projects: converting lectures to Accessibility-friendly captioned videos </li>
+      <li> Selected to present in Brightspace Orientations for Students of UTRGV </li>
+      <li> Selected for Student Outreach Event to spread the news for Brightspace transition </li>
+    </ul>
+
+    <p>&nbsp;</p>
+      <h2 class="center">Digital Media Specialist</h2>
+     <h5 class="center">Twitch Affiliate & Self Owned Artist/Editor</h5>
+
+           <hr class="rounded"></hr>
+           <p>&nbsp;</p>
+     <p class="italic"><b>Description: </b> In charge of promotional endeavors through social media platforms with the use of video production
+      for the entertainment of live audience members. As well as collaborations with other organization members.  </p>
+    <p>&nbsp;</p>
+    <ul>
+      <li> Designs and executes live video streaming content on Twitch.tv  </li>
+      <li> Edits and publishes video content on social media outlets.  </li>
+      <li> Builds and keeps an engaged social media following.  </li>
+      <li> Graphic design and promotional marketing to drive live event attendance and receive paid art commissions.  </li>
+    </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
